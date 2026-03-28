c9009ee600d5ffd91066c1f700cc671a1767ce65
Rishikesh Kumar Singh <151398185+rishikeshsingh906@users.noreply.github.com>
2026-03-28 07:58:48 +0000

    Sync LeetCode submission Runtime - 357 ms (59.50%), Memory - 0.0B (100.00%)

--- 0570-managers-with-at-least-5-direct-reports/solution.sql
+++ 0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -1,9 +1,6 @@
-SELECT name
-FROM Employee
-WHERE id IN (
-    SELECT managerId
-    FROM Employee
-    GROUP BY managerId
-    HAVING COUNT(*) >= 5
-);
-
+SELECT e1.name
+FROM Employee e1
+JOIN Employee e2
+ON e1.id = e2.managerId
+GROUP BY e1.id, e1.name
+HAVING COUNT(e2.id) >= 5;
